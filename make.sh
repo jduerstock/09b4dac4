e0eb29469d0c4c80feaa73a56ac0abc9a2be550e
Jason Duerstock <jason.duerstock@gmail.com>
2013-07-09 18:42:54 +1100

    byte -> ascii, fix some make warnings

--- make.sh
+++ make.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 powerpc-linux-gnu-as -g -o USBHubDriver0.o USBHubDriver0.a || exit
-powerpc-linux-gnu-ld --oformat=binary -Ttext=0xffdf8ea0 -e ffdf8ea0 -o USBHubDriver0.bin USBHubDriver0.o || exit
+powerpc-linux-gnu-ld --oformat=binary -Ttext=0xffdf8ea0 -e 0xffdf8ea0 -o USBHubDriver0.bin USBHubDriver0.o || exit
 powerpc-linux-gnu-as -g -o USBHubDriver1.o USBHubDriver1.a || exit
-powerpc-linux-gnu-ld --oformat=binary -Ttext=0xffe02280 -e ffe02280 -o USBHubDriver1.bin USBHubDriver1.o || exit
+powerpc-linux-gnu-ld --oformat=binary -Ttext=0xffe02280 -e 0xffe02280 -o USBHubDriver1.bin USBHubDriver1.o || exit
 powerpc-linux-gnu-as -g -o USBHIDKeyboardModule.o USBHIDKeyboardModule.a || exit
-powerpc-linux-gnu-ld --oformat=binary -Ttext=0xffe0b640 -e ffe0b640 -o USBHIDKeyboardModule.bin USBHIDKeyboardModule.o || exit
+powerpc-linux-gnu-ld --oformat=binary -Ttext=0xffe0b640 -e 0xffe0b640 -o USBHIDKeyboardModule.bin USBHIDKeyboardModule.o || exit
 powerpc-linux-gnu-as -g -o USBHIDMouseModule.o USBHIDMouseModule.a || exit
-powerpc-linux-gnu-ld --oformat=binary -Ttext=0xffe0e520 -e ffe0e520 -o USBHIDMouseModule.bin USBHIDMouseModule.o || exit
+powerpc-linux-gnu-ld --oformat=binary -Ttext=0xffe0e520 -e 0xffe0e520 -o USBHIDMouseModule.bin USBHIDMouseModule.o || exit
 powerpc-linux-gnu-as -g -o USBCompositeDriver.o USBCompositeDriver.a || exit
 powerpc-linux-gnu-ld --oformat=binary -Ttext=0xffe10e10 -e 0xffe10e10 -o USBCompositeDriver.bin USBCompositeDriver.o || exit
 powerpc-linux-gnu-as -g -o USBUnitTableStorageDriver.o USBUnitTableStorageDriver.a || exit
