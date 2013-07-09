2633d24aebcbef5a3498116f3c51299a27734d9b
Jason Duerstock <jason.duerstock@gmail.com>
2013-07-09 18:04:10 +1100

    split phase 2

--- make.sh
+++ make.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+powerpc-linux-gnu-as -g --statistics -o USBHIDMouseModule.o USBHIDMouseModule.a || exit
+powerpc-linux-gnu-ld --stats --oformat=binary -Ttext=0xffe0e520 -e ffe0e520 -o USBHIDMouseModule.bin USBHIDMouseModule.o || exit
 powerpc-linux-gnu-as -g --statistics -o USBCompositeDriver.o USBCompositeDriver.a || exit
 powerpc-linux-gnu-ld --stats --oformat=binary -Ttext=0xffe10e10 -e 0xffe10e10 -o USBCompositeDriver.bin USBCompositeDriver.o || exit
 powerpc-linux-gnu-as -g --statistics -o USBUnitTableStorageDriver.o USBUnitTableStorageDriver.a || exit
