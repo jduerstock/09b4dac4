f9013c7151dd7bd3849ffc012506987c9ad0304c
Jason Duerstock <jason.duerstock@gmail.com>
2013-07-09 12:49:59 +1100

    split out some resources

--- make.sh
+++ make.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 powerpc-linux-gnu-as -g --statistics -o USBMassStorageVSDriver.o USBMassStorageVSDriver.a || exit
 powerpc-linux-gnu-ld --stats --oformat=binary -Ttext=0xffe2aab0 -e 0xffe2aab0 -o USBMassStorageVSDriver USBMassStorageVSDriver.o || exit
+powerpc-linux-gnu-as -g --statistics -o USBMassStorageClassDriver.o USBMassStorageClassDriver.a || exit
+powerpc-linux-gnu-ld --stats --oformat=binary -Ttext=0xffe2aab0 -e 0xffe2aab0 -o USBMassStorageClassDriver USBMassStorageClassDriver.o || exit
 powerpc-linux-gnu-as -g --statistics -o 09b4dac4.o 09b4dac4.a || exit
 powerpc-linux-gnu-ld --stats --oformat=binary -Ttext=0xffc00000 -e 0xffc00000 -o bork 09b4dac4.o || exit
 sha1sum bork 09b4dac4.bin
