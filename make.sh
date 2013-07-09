4ec9a89b43ab6d57fc29963b2f433e772343a784
Jason Duerstock <jason.duerstock@gmail.com>
2013-07-09 17:57:59 +1100

    incbin files use .bin now one day i will fix this ugly make script

--- make.sh
+++ make.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+powerpc-linux-gnu-as -g --statistics -o USBCompositeDriver.o USBCompositeDriver.a || exit
+powerpc-linux-gnu-ld --stats --oformat=binary -Ttext=0xffe10e10 -e 0xffe10e10 -o USBCompositeDriver.bin USBCompositeDriver.o || exit
 powerpc-linux-gnu-as -g --statistics -o USBUnitTableStorageDriver.o USBUnitTableStorageDriver.a || exit
-powerpc-linux-gnu-ld --stats --oformat=binary -Ttext=0xffe12e10 -e 0xffe12e10 -o USBUnitTableStorageDriver USBUnitTableStorageDriver.o || exit
+powerpc-linux-gnu-ld --stats --oformat=binary -Ttext=0xffe12e10 -e 0xffe12e10 -o USBUnitTableStorageDriver.bin USBUnitTableStorageDriver.o || exit
 powerpc-linux-gnu-as -g --statistics -o USBMassStorageVSDriver.o USBMassStorageVSDriver.a || exit
-powerpc-linux-gnu-ld --stats --oformat=binary -Ttext=0xffe26c00 -e 0xffe26c00 -o USBMassStorageVSDriver USBMassStorageVSDriver.o || exit
+powerpc-linux-gnu-ld --stats --oformat=binary -Ttext=0xffe26c00 -e 0xffe26c00 -o USBMassStorageVSDriver.bin USBMassStorageVSDriver.o || exit
 powerpc-linux-gnu-as -g --statistics -o USBMassStorageClassDriver.o USBMassStorageClassDriver.a || exit
-powerpc-linux-gnu-ld --stats --oformat=binary -Ttext=0xffe2aab0 -e 0xffe2aab0 -o USBMassStorageClassDriver USBMassStorageClassDriver.o || exit
+powerpc-linux-gnu-ld --stats --oformat=binary -Ttext=0xffe2aab0 -e 0xffe2aab0 -o USBMassStorageClassDriver.bin USBMassStorageClassDriver.o || exit
 powerpc-linux-gnu-as -g --statistics -o 09b4dac4.o 09b4dac4.a || exit
 powerpc-linux-gnu-ld --stats --oformat=binary -Ttext=0xffc00000 -e 0xffc00000 -o bork 09b4dac4.o || exit
 sha1sum bork 09b4dac4.bin
